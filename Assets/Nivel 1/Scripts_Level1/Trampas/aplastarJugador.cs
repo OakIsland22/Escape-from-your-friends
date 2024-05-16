8f0d95b5eb8a9d7378f7e214b7cf5feeb92955fc
MATMaucio <marellanotsimogianis@gmail.com>
2024-05-16 12:59:24 -0600

    trampa terminada

--- Assets/Nivel 1/Scripts_Level1/Trampas/aplastarJugador.cs
+++ Assets/Nivel 1/Scripts_Level1/Trampas/aplastarJugador.cs
@@ -12,18 +12,35 @@ public class aplastarJugador : MonoBehaviour
 
     public bool estaEnTrampaP2 = false;
     public bool estaEnTrampaP3 = false;
+    public bool objetoCayendo = false;
+    public float velocidadCaida = 10f;
 
     public Activar_trampa activar_Trampa;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player_2") && activar_Trampa == true)
+        if (other.gameObject.CompareTag("Player_2") && activar_Trampa.activarTrampa == true)
         {
             estaEnTrampaP2 = true;
             DanoPlayer2();
         }
 
-        if (other.gameObject.CompareTag("Player_3") && activar_Trampa == true)
+        if (other.gameObject.CompareTag("Player_3") && activar_Trampa.activarTrampa == true)
+        {
+            estaEnTrampaP3 = true;
+            DanoPlayer3();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player_2") && activar_Trampa.activarTrampa == true)
+        {
+            estaEnTrampaP2 = true;
+            DanoPlayer2();
+        }
+
+        if (other.gameObject.CompareTag("Player_3") && activar_Trampa.activarTrampa == true)
         {
             estaEnTrampaP3 = true;
             DanoPlayer3();
@@ -32,10 +49,10 @@ public class aplastarJugador : MonoBehaviour
 
     public void DanoPlayer2()
     {
-        if(estaEnTrampaP2 == true)
+        if (estaEnTrampaP2 == true)
         {
             vida_P2.TomarDano(DanoTrampa);
-        }        
+        }
     }
 
     public void DanoPlayer3()
@@ -43,6 +60,14 @@ public class aplastarJugador : MonoBehaviour
         if (estaEnTrampaP3 == true)
         {
             vida_P3.TomarDano(DanoTrampa);
+        }
+    }
+
+    private void Update()
+    {
+        if (activar_Trampa.activarTrampa == true)
+        { 
+            transform.Translate(Vector3.down * velocidadCaida * Time.deltaTime);
         }
     }
 }
