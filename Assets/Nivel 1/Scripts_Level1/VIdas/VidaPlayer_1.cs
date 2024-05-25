c7719cbbc19b5aa49ddef1899767cc3482b62122
MATMaucio <marellanotsimogianis@gmail.com>
2024-05-25 16:10:16 -0600

    cambios

--- Assets/Nivel 1/Scripts_Level1/VIdas/VidaPlayer_1.cs
+++ Assets/Nivel 1/Scripts_Level1/VIdas/VidaPlayer_1.cs
@@ -10,15 +10,23 @@ public class VidaPlayer_1 : MonoBehaviour
 {
     public float vida;
     public int vidaMaxima = 6;
-    
+
     public GameObject Perdiste_P1;
 
     public bool Moriste = false;
 
+    public float shakeDuration = 0.1f;
+    public float shakeMagnitude = 0.1f;
+
+    private Vector3 originalPosition;
+    private float shakeTimer = 0f;
+
     private void Start()
     {
         vida = vidaMaxima;
+        originalPosition = transform.position;
     }
+
     private void Update()
     {
         Dead();
@@ -27,15 +35,28 @@ public class VidaPlayer_1 : MonoBehaviour
         {
             vida = 0;
         }
+
+        if (shakeTimer > 0)
+        {
+            transform.position = originalPosition + Random.insideUnitSphere * shakeMagnitude;
+            shakeTimer -= Time.deltaTime;
+        }
+        else
+        {
+            shakeTimer = 0f;
+            transform.position = originalPosition;
+        }
     }
+
     public void TomarDano(float dano)
     {
-        vida -= dano; 
+        vida -= dano;
+        shakeTimer = shakeDuration;
     }
 
     public void Dead()
     {
-        if(vida == 0)
+        if (vida == 0)
         {
             Perdiste_P1.gameObject.SetActive(true);
             Moriste = true;
--- Assets/Nivel 1/Scripts_Level1/VIdas/VidaPlayer_3.cs
+++ Assets/Nivel 1/Scripts_Level1/VIdas/VidaPlayer_3.cs
@@ -11,22 +11,32 @@ public class VidaPlayer_3 : MonoBehaviour
     public GameObject Perdiste_P2;
 
     public bool Moriste = false;
+
+    public float shakeDuration = 0.1f;
+    public float shakeMagnitude = 0.1f;
+
+    private Vector3 originalPosition;
+
     private void Start()
     {
         vida = vidaMaxima;
+        originalPosition = transform.position;
     }
+
     private void Update()
     {
         Dead();
 
-        if(vida <= 0)
+        if (vida <= 0)
         {
             vida = 0;
         }
     }
+
     public void TomarDano(float dano)
     {
-        vida -= dano;       
+        vida -= dano;
+        StartCoroutine(ShakeCamera());
     }
 
     public void Dead()
@@ -36,5 +46,24 @@ public class VidaPlayer_3 : MonoBehaviour
             Perdiste_P2.gameObject.SetActive(true);
             Moriste = true;
         }
+    }
+
+    private IEnumerator ShakeCamera()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            transform.position = originalPosition + new Vector3(x, y, 0f);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = originalPosition;
     }
 }
